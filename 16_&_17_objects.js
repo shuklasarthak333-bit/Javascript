bc5083a75dac10602fce1350086cb5b2c5d6719c
shuklasarthak333-bit <shuklasarthak333@gmail.com>
2026-09-19 00:44:35 +0000

    objects

--- 16_&_17_objects.js
+++ 16_&_17_objects.js
@@ -47,3 +47,47 @@ console.log(Obj.greeting0());
 //------- part 1 ends --------
 
 // ----- part 2 start -----
+
+// Nesting of Objects
+
+const credentials = {
+    fullname : {
+        firstName : "Sarthak",
+        lastName : "Shukla",
+        Special:{
+            nickname : "Impala"
+        }
+    },
+    Section : 24,
+    Age : 20 
+};
+
+console.log(credentials.fullname.Special.nickname);
+
+// merging two or more objects
+
+const obj1 = {
+    1:"abc",
+    2:"xyz"
+};
+const obj2 = {
+    3:"abc",
+    4:"xyz"
+};
+const obj3 = {
+    5:"abc",
+    6:"xyz"
+};
+
+const obj4=(obj1,obj2,obj3);
+console.log(obj4);
+const obj5 = Object.assign({},obj1,obj2,obj3) // assign function
+console.log(obj5);
+const obj6 = {...obj1, ...obj2, ...obj3}; // spread
+console.log(obj6);
+
+ // to print all the keys and values in form of an array
+
+console.log(Object.keys(obj5));
+console.log(Object.values(obj5));
+console.log(Object.entries(obj5)); // ---> to give the key and value pairs inside an array
